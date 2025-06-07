1d2251ead60fe5d4f048733421556be3163568d5
unknown <hyh011025@gmail.com>
2025-06-07 15:26:24 +0800

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/root/my-website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/root/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/root/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/root/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/root/my-website/src/css/custom.css"),
+];
---
+++ src/pages/index.tsx
@@ -0,0 +1,53 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+import Mywords from './_mywords.md';
+import MyBell from './ring-the-bell.html';
+
+
+export default function Home(): ReactNode {
+  const {siteConfig} = useDocusaurusContext();
+  
+  return (
+    <Layout
+        title = 'BlogYijiong'
+        description="Congrads, you found it.">
+        <HomepageHeader />
+         <div className={styles.markdownContainer}>
+            <Mywords/>
+            <div>
+               put a bel here to let you ring
+            </div>
+        </div>
+
+    </Layout>
+  );
+}
+
